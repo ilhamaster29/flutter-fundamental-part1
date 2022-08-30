2f2ebcedcb1ab821448764bb1eac4cebdebae6be
Ilham Nurcahyono <ilhamaster29@gmail.com>
2022-08-30 07:43:06 +0700

    tambah widget

--- lib/basic_widgets/fab_widget.dart
+++ lib/basic_widgets/fab_widget.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class MyTextWidget extends StatelessWidget {
-  const MyTextWidget({Key? key}) : super(key: key);
+class FABWidget extends StatelessWidget {
+  const FABWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
